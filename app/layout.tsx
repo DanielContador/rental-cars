9f57148e07fbabf25c8514a5b9943b179d64c921
Daniel Contador <danielcontadorb@gmail.com>
2024-10-18 16:45:09 -0300

    stripe intento

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
 import {  ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from "@/components/ui/toaster";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 const outfit = Outfit({ subsets: ["latin"]});
 
@@ -19,13 +20,15 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="es">
-      <body className={outfit.className}>
-        <NextTopLoader color="#000"/>
-        {children}
-        <Toaster/>
-      </body>
-    </html>  
-    </ClerkProvider> 
+      <PayPalScriptProvider options={{"clientId":"AYj-WBuTRhZOEeflKhKZzzFu2oWlaLp4hDnknCpJc7xuO5Ghz6PZR-I_ITfwPRr11NVj5tS1WiRyHp-y"}}>
+        <html lang="es">
+          <body className={outfit.className}>
+            <NextTopLoader color="#000"/>
+            {children}
+            <Toaster/>
+          </body>
+        </html>  
+      </PayPalScriptProvider>
+    </ClerkProvider>  
   );
 }
