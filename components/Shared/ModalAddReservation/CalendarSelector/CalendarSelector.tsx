b108bad6692593b2fe0561758a30fbf4d6395332
DanielContador <102329605+DanielContador@users.noreply.github.com>
2024-10-30 21:10:40 -0300

    validar fechas

--- components/Shared/ModalAddReservation/CalendarSelector/CalendarSelector.tsx
+++ components/Shared/ModalAddReservation/CalendarSelector/CalendarSelector.tsx
@@ -1,88 +1,90 @@
-"use client"
-import * as React from 'react'
+"use client";
+import * as React from 'react';
 import { CalendarIcon } from 'lucide-react';
-import { addDays, format } from 'date-fns';
+import { addDays, format, isBefore, startOfToday } from 'date-fns';
 import { DateRange } from 'react-day-picker';
 import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { CalendarSelectorProps } from "./CalendarSelector.types";
-import { useState , useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { cn } from '@/lib/utils';
 
 export function CalendarSelector(props: CalendarSelectorProps) {
   const { setDateSelected, className, autoPrecioDia } = props;
-  const [ date, setDate] = useState<DateRange | undefined>({
-    from : new Date,
-    to: addDays(new Date(), 5)
+  const [date, setDate] = useState<DateRange | undefined>({
+    from: startOfToday(),
+    to: addDays(startOfToday(), 5)
   });
 
   useEffect(() => {
     setDateSelected({
-      from : date?.from,
+      from: date?.from,
       to: date?.to,
     });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [date]);
+  }, [date, setDateSelected]);
 
-  const calculateDaysBetween = (from: Date, to:Date): number => {
+  const calculateDaysBetween = (from: Date, to: Date): number => {
     const oneDay = 24 * 60 * 60 * 1000;
     const diffInTime = to.getTime() - from.getTime();
     return Math.round(diffInTime / oneDay);
   };
 
-  const daysBetween = 
-    date?.from && date?.to ? calculateDaysBetween(date.from, date.to) : 0;
+  const daysBetween = date?.from && date?.to ? calculateDaysBetween(date.from, date.to) : 0;
+
+  // Function to disable past dates
+  const isDateDisabled = (date: Date) => {
+    return isBefore(date, startOfToday());
+  };
 
   return (
     <>
-    <div className={cn("grid gap-2", className)}>
-      {date?.from && date?.to && (
-        <>
-          <p className="mt-4 text-lg text-black">
-            Dias totales: {daysBetween}
-          </p>
-          <p className="mb-4 text-md">
-            Precio total: {daysBetween * Number(autoPrecioDia)} CLP$ (Imp.incluidos)
-          </p>
-        </>
-      )}
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant="outline"
-            className={cn("justify-start text-left font-normal", !date && "text-muted-foreground" )}
+      <div className={cn("grid gap-2", className)}>
+        {date?.from && date?.to && (
+          <>
+            <p className="mt-4 text-lg text-black">
+              Días totales: {daysBetween}
+            </p>
+            <p className="mb-4 text-md">
+              Precio total: {daysBetween * Number(autoPrecioDia)} CLP$ (Imp.incluidos)
+            </p>
+          </>
+        )}
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              id="date"
+              variant="outline"
+              className={cn("justify-start text-left font-normal", !date && "text-muted-foreground")}
             >
-              <CalendarIcon  className="w-4 h-4 mr-2" />
-
+              <CalendarIcon className="w-4 h-4 mr-2" />
               {date?.from ? (
                 date.to ? (
                   <>
-                  {format(date.from, "LLL dd, y")} - {""}
-                  {format(date.to, "LLL dd, y")}
+                    {format(date.from, "LLL dd, y")} - {""}
+                    {format(date.to, "LLL dd, y")}
                   </>
                 ) : (
                   format(date.from, "LLL dd, y")
                 )
-              ): (
+              ) : (
                 <span>Elige tu fecha</span>
               )}
             </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
-          <Calendar
-            initialFocus
-            mode='range'
-            defaultMonth={date?.from}
-            selected={date}
-            onSelect={setDate}
-            numberOfMonths={2}
-          />
-        </PopoverContent>
-      </Popover>
-
-    </div>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar
+              initialFocus
+              mode='range'
+              defaultMonth={date?.from}
+              selected={date}
+              onSelect={setDate}
+              numberOfMonths={2}
+              disabled={isDateDisabled}
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
     </>
-  )
+  );
 }
