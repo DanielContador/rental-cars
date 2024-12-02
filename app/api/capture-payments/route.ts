6c7e7cb78a67d29fc67c79488eb11ea317dc47e3
DanielContador <102329605+DanielContador@users.noreply.github.com>
2024-12-02 19:39:36 -0300

    asda

--- app/api/capture-payments/route.ts
+++ app/api/capture-payments/route.ts
@@ -1,24 +1,25 @@
 /* eslint-disable */
+
+// capture-payments.js
 import { NextRequest, NextResponse } from 'next/server';
 import { db } from '@/lib/db';
 const paypal = require('@paypal/checkout-server-sdk');
 
-const clientId = process.env.PAYPAL_CLIENT_ID!;
-const clientSecret = process.env.PAYPAL_CLIENT_SECRET!;
+const clientId = process.env.PAYPAL_CLIENT_ID;
+const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
 const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
 const payPalClient = new paypal.core.PayPalHttpClient(environment);
 
+
 export async function GET(req: NextRequest) {
-  const { searchParams } = req.nextUrl; // Utiliza nextUrl para obtener searchParams
+  
+  const { searchParams } = new URL(req.url);
   const token = searchParams.get('token');
   const payerId = searchParams.get('PayerID');
   const orderid = searchParams.get('ordenID');
 
   if (!token || !payerId || !orderid) {
-    return NextResponse.json(
-      { success: false, error: 'Token, PayerID, and orderId are required' },
-      { status: 400 }
-    );
+    return NextResponse.json({ success: false, error: 'Token, PayerID, and orderId are required' }, { status: 400 });
   }
 
   const capturePaymentRequest = new paypal.orders.OrdersCaptureRequest(token);
@@ -27,17 +28,15 @@ export async function GET(req: NextRequest) {
   try {
     const response = await payPalClient.execute(capturePaymentRequest);
     await db.orden.update({
-      where: { id: orderid },
-      data: { estado: 'confirmed' },
+      where: { id: orderid }, // Aquí buscamos la orden por su ID
+      data: { estado: 'confirmed' }, // Cambiamos el estado a "confirmed"
     });
-    return NextResponse.redirect(
-      `${process.env.NEXT_PUBLIC_FRONTEND_STORE_URL}/order-confirmation?success=true`
-    );
+    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_FRONTEND_STORE_URL}/order-confirmation?success=true`);
+
+
+
   } catch (error) {
     console.error('Error capturing payment:', error);
-    return NextResponse.json(
-      { success: false, error: 'Internal server error' },
-      { status: 500 }
-    );
+    return NextResponse.json({ success: false, error: 'Internal server error' }, { status: 500 });
   }
 }
