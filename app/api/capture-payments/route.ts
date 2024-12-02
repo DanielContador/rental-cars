c51a59c6af3d025ddece8c59111e3345f215b77b
DanielContador <102329605+DanielContador@users.noreply.github.com>
2024-12-02 19:34:33 -0300

    aa

--- app/api/capture-payments/route.ts
+++ app/api/capture-payments/route.ts
@@ -1,6 +1,42 @@
 /* eslint-disable */
-import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(_:any) {
-  return NextResponse.json({ success: true, message: 'Ruta básica funcional' });
+// capture-payments.js
+import { NextResponse } from 'next/server';
+import { db } from '@/lib/db';
+const paypal = require('@paypal/checkout-server-sdk');
+
+const clientId = process.env.PAYPAL_CLIENT_ID;
+const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
+const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
+const payPalClient = new paypal.core.PayPalHttpClient(environment);
+
+
+export async function GET(req: { url: string | URL; }) {
+  
+  const { searchParams } = new URL(req.url);
+  const token = searchParams.get('token');
+  const payerId = searchParams.get('PayerID');
+  const orderid = searchParams.get('ordenID');
+
+  if (!token || !payerId || !orderid) {
+    return NextResponse.json({ success: false, error: 'Token, PayerID, and orderId are required' }, { status: 400 });
+  }
+
+  const capturePaymentRequest = new paypal.orders.OrdersCaptureRequest(token);
+  capturePaymentRequest.requestBody({});
+
+  try {
+    const response = await payPalClient.execute(capturePaymentRequest);
+    await db.orden.update({
+      where: { id: orderid }, // Aquí buscamos la orden por su ID
+      data: { estado: 'confirmed' }, // Cambiamos el estado a "confirmed"
+    });
+    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_FRONTEND_STORE_URL}/order-confirmation?success=true`);
+
+
+
+  } catch (error) {
+    console.error('Error capturing payment:', error);
+    return NextResponse.json({ success: false, error: 'Internal server error' }, { status: 500 });
+  }
 }
